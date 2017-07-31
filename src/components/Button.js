077e1a7d3d7eb01535bab84c4bb1cfe488a24873
banafsheh <banasheh.rahnama@metropolia.fi>
2017-07-31 23:16:04 +0300

    icon changed

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,7 +5,7 @@ export default class Button extends Component {
   render() {
     return (
       <button className={this.props.className} onClick={this.props.onClick}>
-        <i className="fa fa-check" aria-hidden="true"></i>
+        <i className="fa fa-angle-double-right" aria-hidden="true"></i>
       </button>
     )
   }
