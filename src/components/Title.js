c20157038bfe0fca4cd226d32a335e37e913918f
banafsheh <banasheh.rahnama@metropolia.fi>
2017-07-31 23:14:26 +0300

    styles added

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
+import './Title.css'
 
 export default class Title extends Component {
   render() {
-    return <h1>{this.props.value}</h1>
+    return <h3>{this.props.value}</h3>
   }
 }
--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -1,11 +1,14 @@
 import React, {Component} from 'react';
+import './Todolist.css'
 
 export default class Todolist extends Component {
   render() {
+    const {children} = this.props
     return (
-      <span href="#" className={this.props.className} onClick={this.props.onClick}>
+      <div href="#" className={this.props.className} onClick={this.props.onClick}>
         {this.props.value}
-      </span>
+        {children}
+      </div>
     )
   }
 }
